734ed05cacd432dc8bea0eeea9122a293e61e170
Mohamed-Dawood9 <mboa990@gmail.com>
2024-12-17 00:16:45 +0200

    Add project files.

---
+++ GP.DAL/Data/Models/Doctor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP.DAL.Data.Models
+{
+	public class Doctor : ModelBase
+	{
+	}
+}
---
+++ GP.DAL/Data/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP.DAL.Data.Models
+{
+	public class Patient:ModelBase
+	{
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Gender { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Height { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string MedicalHistory { get; set; }
+    }
+}
